06db1d9acdd77b36ec80f42fd0294fc0356c22fd
sriram-555-savage <pokurisriram55@gmail.com>
2025-01-05 04:50:42 -0500

    Add files via upload

---
+++ js/datatypes.js
@@ -0,0 +1,8 @@
+//tc39
+//md9
+//symbol
+let name="sriram"
+let age=19
+let diver=false
+console.log(name)
+console.log(typeof age)
---
+++ js/first.js
@@ -0,0 +1,5 @@
+let x=5
+const accountId=10
+let accpasswar="Sriram@0517"
+console.log(x+accountId)
+console.log(accpasswar)
